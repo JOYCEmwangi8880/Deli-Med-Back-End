34adbb98334f45e7ffa948843b94a02db33413e8
leongitonga <leon.kai.33@gmail.com>
2024-01-23 14:36:57 +0300

    second models

--- server/models.py
+++ server/models.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'Users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -13,10 +14,10 @@ class User(db.Model):
     blood_type = db.Column(db.String(3))
     previous_illnesses = db.relationship('Illness', secondary='user_illness')
 
-    previous_illnesses = db.Column(db.String(300))  
+
+    previous_illnesses = db.Column(db.String(750))  
 
     def add_previous_illness(self, illness_name):
-        
         if self.previous_illnesses:
             self.previous_illnesses += f',{illness_name}'
         else:
@@ -24,31 +25,34 @@ class User(db.Model):
 
 
 class Illness(db.Model):
+    __tablename__ = 'Illnesses'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     description = db.Column(db.Text)
     medicines = db.relationship('Medicine', secondary='illness_medicine')
 
 class Medicine(db.Model):
+    __tablename__ = 'Medicines'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     description = db.Column(db.Text)
     price = db.Column(db.Float)
 
 class Order(db.Model):
+    __tablename__ = 'Orders'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    medicine_id = db.Column(db.Integer, db.ForeignKey('medicine.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    medicine_id = db.Column(db.Integer, db.ForeignKey('medicines.id'), nullable=False)
     quantity = db.Column(db.Integer)
     total_price = db.Column(db.Float)
     delivery_address = db.Column(db.String(200))
 
 user_illness = db.Table('user_illness',
-    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-    db.Column('illness_id', db.Integer(), db.ForeignKey('illness.id'))
+    db.Column('user_id', db.Integer(), db.ForeignKey('users.id')),
+    db.Column('illness_id', db.Integer(), db.ForeignKey('illnesses.id'))
 )
 
 illness_medicine = db.Table('illness_medicine',
-    db.Column('illness_id', db.Integer(), db.ForeignKey('illness.id')),
-    db.Column('medicine_id', db.Integer(), db.ForeignKey('medicine.id'))
+    db.Column('illness_id', db.Integer(), db.ForeignKey('illnesses.id')),
+    db.Column('medicine_id', db.Integer(), db.ForeignKey('medicines.id'))
 )
