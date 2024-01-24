0ee11dd8d1e9b9730afb7b339d79324b9831adff
Joyce Mwangi <105669036+JOYCEmwangi8880@users.noreply.github.com>
2024-01-24 07:07:48 -0100

    progress

--- server/models.py
+++ server/models.py
@@ -14,22 +14,14 @@ class User(db.Model):
     blood_type = db.Column(db.String(3))
     previous_illnesses = db.relationship('Illness', secondary='user_illness')
 
-
-    previous_illnesses = db.Column(db.String(750))  
-
-    def add_previous_illness(self, illness_name):
-        if self.previous_illneses:
-            self.previous_illneses += f',{illness_name}'
-        else:
-            self.previous_illneses = illness_name
-
-
 class Illness(db.Model):
-    __tablename__ = 'illneses'
+    __tablename__ = 'illnesses'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     description = db.Column(db.Text)
-    medicines = db.relationship('Medicine', secondary='illness_medicine')
+    medicines = db.relationship('Medicine', secondary='illness_medicine',
+                                primaryjoin='Illness.id==illness_medicine.c.illness_id',
+                                secondaryjoin='Medicine.id==illness_medicine.c.medicine_id')
 
 class Medicine(db.Model):
     __tablename__ = 'medicines'
@@ -41,20 +33,18 @@ class Medicine(db.Model):
 class Order(db.Model):
     __tablename__ = 'orders'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     quantity = db.Column(db.Integer)
     total_price = db.Column(db.Float)
     delivery_address = db.Column(db.String(200))
-    medicine_id = db.Column(db.Integer, db.ForeignKey('Medicines.id'), nullable=False)
+    medicine_id = db.Column(db.Integer, db.ForeignKey('medicines.id'), nullable=False)
 
 user_illness = db.Table('user_illness',
-
-    db.Column('user_id', db.Integer(), db.ForeignKey('Users.id')),
-    db.Column('illness_id', db.Integer(), db.ForeignKey('Illnesses.id'))
+    db.Column('user_id', db.Integer(), db.ForeignKey('users.id')),
+    db.Column('illness_id', db.Integer(), db.ForeignKey('illnesses.id'))
 )
 
 illness_medicine = db.Table('illness_medicine',
-    db.Column('illness_id', db.Integer(), db.ForeignKey('Illnesses.id')),
-    db.Column('medicine_id', db.Integer(), db.ForeignKey('Medicines.id'))
+    db.Column('illness_id', db.Integer(), db.ForeignKey('illnesses.id')),
+    db.Column('medicine_id', db.Integer(), db.ForeignKey('medicines.id'))
 )
-
