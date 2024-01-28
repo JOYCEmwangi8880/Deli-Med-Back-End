6c8f51957b821e9bf8e32f31561f476758ae24b5
fabian <farbean.makini@student.moringaschool.com>
2024-01-28 07:42:32 -0800

    saturday

--- server/models.py
+++ server/models.py
@@ -1,8 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+
 db = SQLAlchemy()
 
-class User(db.Model, UserMixin):
+class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -12,27 +12,19 @@ class User(db.Model, UserMixin):
     age = db.Column(db.Integer)
     height = db.Column(db.Float)
     blood_type = db.Column(db.String(3))
-    previous_illnesses = db.Column(db.String(750)) 
-    
-    def is_active(self):
-        return True
+    previous_illnesses = db.Column(db.Text)  # Define 'previous_illnesses' as a Text column
+
+    illnesses = db.relationship('Illness', secondary='user_illness', backref='users')
+
+
 class Illness(db.Model):
     __tablename__ = 'illnesses'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     description = db.Column(db.Text)
-    medicines = db.relationship('Medicine', secondary='illness_medicine',
-                                primaryjoin='Illness.id==illness_medicine.c.illness_id',
-                                secondaryjoin='Medicine.id==illness_medicine.c.medicine_id')
-
-    def serialize(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'description': self.description,
-            'medicines': [medicine.serialize() for medicine in self.medicines]
-        }
-
+    
+    # Define relationship with medicines
+    medicines = db.relationship('Medicine', secondary='illness_medicine', backref='illnesses')
 
 class Medicine(db.Model):
     __tablename__ = 'medicines'
@@ -40,15 +32,6 @@ class Medicine(db.Model):
     name = db.Column(db.String(120), nullable=False)
     description = db.Column(db.Text)
     price = db.Column(db.Float)
-
-    def serialize(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'description': self.description,
-            'price': self.price
-        }
-
 
 class Order(db.Model):
     __tablename__ = 'orders'
